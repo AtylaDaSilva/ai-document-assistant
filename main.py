3a3183123a9ea6cb88543d91280832871a67bd6a
Atyla da Silva <atylamendes97@gmail.com>
2026-04-20 19:25:27 -0300

    MVP WIP 3
    Implemented file-type-agnostic document loading

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from langchain_community.document_loaders.pdf import PyPDFDirectoryLoader
+import langchain_community.document_loaders as lc_doc_loader
 from langchain_ollama.embeddings import OllamaEmbeddings
 from langchain_core.documents import Document
 from langchain_chroma import Chroma
@@ -7,6 +7,20 @@ from typing import Final, Any
 from enum import Enum
 import dotenv
 import os
+from pathlib import Path
+
+
+class EnumFileLoaders(Enum):
+    """Loaders for supported file types"""
+    PDF = lc_doc_loader.PyPDFLoader
+    CSV = lc_doc_loader.CSVLoader  # TODO: test loader
+    JSON = lc_doc_loader.JSONLoader  # TODO: test loader
+    HTML = lc_doc_loader.BSHTMLLoader  # TODO: test loader
+
+
+def get_file_loader(file: Path) -> lc_doc_loader.BaseLoader:
+    file_ext = file.suffix.replace(".", "").upper()
+    return EnumFileLoaders[file_ext]
 
 
 class EnumEmbeddingMethods(Enum):
@@ -24,20 +38,33 @@ def initialize_embeddings(embedding_method: str, embedding_func_args: dict[str, 
 def initialize_vector_store(vector_store_name: str, vector_store_args: dict[str, Any]):
     return EnumVectorStores[vector_store_name].value(**vector_store_args)
 
+
 def lazy_load_documents(documents_path: str) -> list[Document]:
+    """
+    Lazy-loads documents into memory and returns a list of *Document* objects.
+    See *EnumFileLoaders* for supported file types.
+    :param documents_path: The directory to load documents from.
+    :return: List of Document objects.
+    """
     if not os.path.isdir(documents_path):
         raise DirectoryDoesNotExistError(documents_path)
-    elif not os.listdir(documents_path):
+
+    dir_items: list[str] = os.listdir(documents_path)
+
+    if not dir_items:
         raise EmptyDirectoryError(documents_path)
 
-    # TODO: iterate through each file in `documents_path` and:
-    # TODO: determine file type and instantiate an appropriate loader for each specific file type
+    docs: list[Document] = []  # Each page of a file is treated as a document
+    for file_name in dir_items:
+        # Get file-appropriate loader
+        file = Path(file_name)
+        loader_enum = get_file_loader(file)
+        doc_loader: lc_doc_loader.BaseLoader = loader_enum.value(f"{documents_path}/{file.name}")
+        # Load docs
+        for page in doc_loader.lazy_load():  # Lazy load works better for large documents
+            docs.append(page)
 
-    # ! TO BE CONTINUED...
-
-    # Lazy load for large documents
-    doc_loader = PyPDFDirectoryLoader(documents_path, mode="page")
-    return [doc for doc in doc_loader.lazy_load()]
+    return docs
 
 def main():
     # Initialize environment variables
@@ -67,6 +94,8 @@ def main():
 
         # Load documents into memory
         documents = lazy_load_documents(ENV["DOCUMENTS_PATH"])
+        # ! TO BE CONTINUED...
+        # Index and add documents to vector store (check if already exists in storage)
     except KeyError:
         # TODO: implement logging
         print("Oops, something went wrong.")
