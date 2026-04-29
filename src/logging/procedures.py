21fa79186900539b49d00562b1ef0d97a82766da
Atyla da Silva <atylamendes97@gmail.com>
2026-04-29 15:02:50 -0300

    FEAT - Logs do not appear on CLI

--- src/logging/procedures.py
+++ src/logging/procedures.py
@@ -3,4 +3,5 @@ from loguru import logger
 def initialize_log_sinks(env: dict[str, str]) -> None:
     sink_dir: str = env["LOG_SINK_DIR"]
     sink_file: str = env["LOG_SINK_FILE"]
+    logger.remove()  # Remove logging to stderr so that logs don't appear on the CLI
     logger.add(sink_dir + "/" + sink_file)
